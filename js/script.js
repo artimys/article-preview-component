935e484ade0843a77f2b6ad1422e64071379231f
Arturo Simon <artysimon@gmail.com>
2020-10-05 20:34:06 -0400

    Remove body event listener, add anchor to social share icons and minor clean cleanup to css

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-controls = {
-    divBottomContent: ".content-bot",
-    classTurnOnSocial: "turn-on-share-options"
-};
-
-document.querySelector("body").addEventListener('click', function() {
-    var bottomContent = document.querySelector(controls.divBottomContent);
-
-    if (bottomContent.classList.contains(controls.classTurnOnSocial)) {
-        bottomContent.classList.remove(controls.classTurnOnSocial);
-    }
-});
-
-
 document.querySelector(".share-button").addEventListener('click', function(e) {
-    var bottomContent = document.querySelector(controls.divBottomContent);
-
-    if (bottomContent.classList.contains(controls.classTurnOnSocial)) {
-        bottomContent.classList.remove(controls.classTurnOnSocial);
-    } else {
-        bottomContent.classList.add(controls.classTurnOnSocial);
-    }
-    e.stopPropagation();
-})
+    document.querySelector(".content-bot").classList.toggle("turn-on-share-options");
+});
